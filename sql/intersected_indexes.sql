ae98a2506f5420687e209143afafda1d9f0d1fcb
ivan.vakhrushev <ivvakhrushev@yandex-team.ru>
2020-03-22 22:35:59 +0300

    Fixed an issued with partial indexes in the check on intersecting indexes.

--- sql/intersected_indexes.sql
+++ sql/intersected_indexes.sql
@@ -5,14 +5,29 @@
  * Licensed under the Apache License 2.0
  */
 
-select a.indrelid::regclass as table_name,
-                        'idx=' || a.indexrelid::regclass || ', size=' || pg_relation_size(a.indexrelid) || '; idx=' ||
-                        b.indexrelid::regclass || ', size=' || pg_relation_size(b.indexrelid) as intersected_indexes
-from (select *, array_to_string(indkey, ' ') as cols from pg_catalog.pg_index) as a
-         join (select *, array_to_string(indkey, ' ') as cols from pg_catalog.pg_index) as b
-              on (a.indrelid = b.indrelid and a.indexrelid > b.indexrelid and (
-                      (a.cols like b.cols || '%' and coalesce(substr(a.cols, length(b.cols) + 1, 1), ' ') = ' ') or
-                      (b.cols like a.cols || '%' and coalesce(substr(b.cols, length(a.cols) + 1, 1), ' ') = ' ')))
-         join pg_catalog.pg_stat_all_indexes psai on a.indexrelid = psai.indexrelid
-where psai.schemaname = :schema_name_param::text
+-- Finds indexes with overlapping sets of columns.
+-- For example, (A) and (A+B) and (A+B+C).
+-- Some of these indexes can usually be safely deleted.
+with index_info as (
+    select
+         pi.indrelid,
+         pi.indexrelid,
+         array_to_string(pi.indkey, ' ') as cols,
+         'idx=' || pi.indexrelid::regclass || ', size=' || pg_relation_size(pi.indexrelid) as info,
+         coalesce(pg_get_expr(pi.indpred, pi.indrelid, true), '') as pred
+    from pg_catalog.pg_index pi
+        join pg_catalog.pg_stat_all_indexes psai on pi.indexrelid = psai.indexrelid
+    where psai.schemaname = :schema_name_param::text
+)
+select
+    a.indrelid::regclass as table_name,
+    a.info || '; ' || b.info as intersected_indexes
+from
+    (select * from index_info) as a
+       join
+    (select * from index_info) as b
+        on (a.indrelid = b.indrelid and a.indexrelid > b.indexrelid and (
+           (a.cols like b.cols || '%' and coalesce(substr(a.cols, length(b.cols) + 1, 1), ' ') = ' ') or
+           (b.cols like a.cols || '%' and coalesce(substr(b.cols, length(a.cols) + 1, 1), ' ') = ' ')) and
+            a.pred = b.pred)
 order by a.indrelid::regclass::text;
