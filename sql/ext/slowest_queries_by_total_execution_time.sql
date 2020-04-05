40d12717388fe70b5454799f8e021c8df73d2b9c
ivan.vakhrushev <ivvakhrushev@yandex-team.ru>
2020-04-05 13:33:27 +0300

    Added query to find the slowest queries by total execution time.

---
+++ sql/ext/slowest_queries_by_total_execution_time.sql
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019-2020. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health-sql
+ *
+ * Licensed under the Apache License 2.0
+ */
+
+-- Finds the slowest queries by total execution time.
+-- Requires the pg_stat_statement extension.
+select
+    round(total_time / 1000 / 60)::integer as total_time_minutes,
+    calls as calls_count,
+    round(mean_time::numeric, 3) as average_time_ms,
+    query
+from pg_stat_statements
+order by total_time desc
+limit :limit_count::integer;
