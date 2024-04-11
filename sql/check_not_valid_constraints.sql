a102d8df1e2ccdbb29e8c0ac715ca97c66a99686
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-04-11 20:55:43 +0900

    request approve

--- sql/check_not_valid_constraints.sql
+++ sql/check_not_valid_constraints.sql
@@ -4,15 +4,16 @@
  *
  * Licensed under the Apache License 2.0
  */
-SELECT
-    t.relname AS table_name, -- Name of the table
-    c.conname AS constraint_name, -- Name of the constraint
-    c.contype AS constraint_type  -- Type of the constraint
-FROM
+
+select
+    t.relname as table_name, -- Name of the table
+    c.conname as constraint_name, -- Name of the constraint
+    c.contype as constraint_type  -- Type of the constraint
+from
     pg_catalog.pg_constraint c
-    JOIN pg_catalog.pg_class t ON t.oid = c.conrelid
-    JOIN pg_catalog.pg_namespace n ON n.oid = t.relnamespace
-WHERE
-    NOT c.convalidated -- Constraints that have not yet been validated
-    AND c.contype IN ('c', 'f') -- Focus on check and foreign key constraints
-    AND n.nspname = :schema_name_param::text; -- Make the query schema-aware
+    join pg_catalog.pg_class t on t.oid = c.conrelid
+    join pg_catalog.pg_namespace n on n.oid = t.relnamespace
+where
+    not c.convalidated -- Constraints that have not yet been validated
+    and c.contype in ('c', 'f') -- Focus on check and foreign key constraints
+    and n.nspname = :schema_name_param::text; -- Make the query schema-aware
