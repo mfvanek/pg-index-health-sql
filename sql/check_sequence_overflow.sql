e189f7d3147f40f862ee4ebf4a57a1647e76deef
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-05-06 00:21:45 +0900

    update :: query

--- sql/check_sequence_overflow.sql
+++ sql/check_sequence_overflow.sql
@@ -5,16 +5,39 @@
  * Licensed under the Apache License 2.0
  */
 
-select
-    schemaname, -- schema name
-    sequencename, -- check sequence growth direction (increment or decrement)
-    case
-        when increment_by > 0
-        then
-            100.0 * (max_value - coalesce(last_value, start_value)) / (max_value - min_value)
-        else
-            100.0 * (coalesce(last_value, start_value) - min_value) / (max_value - min_value)
-    end :: numeric(5, 2) as remaining_percentage -- percentage of remaining values
-from pg_catalog.pg_sequences
-where not cycle -- exclude cyclic sequences
-order by schemaname, sequencename;
+with all_sequences as (
+    select
+        s.seqrelid::regclass::text as sequence_name,
+        s.seqtypid::regtype as data_type,
+        s.seqstart as start_value,
+        s.seqmin as min_value,
+        s.seqmax as max_value,
+        s.seqincrement as increment_by,
+        case when has_sequence_privilege(c.oid, 'select,usage'::text)
+        then pg_sequence_last_value(c.oid::regclass)
+        else null::bigint end as last_value
+    from
+        pg_sequence s
+        join pg_class c on c.oid = s.seqrelid
+        left join pg_namespace n on n.oid = c.relnamespace
+    where
+        not pg_is_other_temp_schema(n.oid) -- not temporary
+        and c.relkind = 'S'::char -- sequence object
+        and not s.seqcycle -- skip cycle sequences
+        and n.nspname = :schema_name_param::text
+),
+sequence_state as (
+    select
+        t.sequence_name,
+        t.data_type,
+        case when t.increment_by > 0 -- ascending or descending sequence
+        then 100.0 * (t.max_value - coalesce(t.last_value, t.start_value)) / (t.max_value - t.min_value)
+        else 100.0 * (coalesce(t.last_value, t.start_value) - t.min_value) / (t.max_value - t.min_value)
+        end ::numeric(5, 2) as remaining_percentage -- percentage of remaining values
+    from all_sequences as t
+)
+select *
+from sequence_state as s
+where
+    s.remaining_percentage <= :remaining_percentage_threshold::numeric(5, 2)
+order by s.sequence_name;
