b17e138f2047a91f7fdd1995324a462f8f2b9bf7
Ivan Vakhrushev <mfvanek@gmail.com>
2025-03-29 19:47:20 +0400

    Update query to find unused indexes

--- sql/unused_indexes.sql
+++ sql/unused_indexes.sql
@@ -10,10 +10,20 @@
 -- The final result can be obtained as an intersection of results from all hosts.
 -- noqa: disable=RF02
 with
+    nsp as (
+        select
+            nsp.oid,
+            nsp.nspname
+        from pg_catalog.pg_namespace nsp
+        where
+            nsp.nspname = :schema_name_param::text
+    ),
+
     foreign_key_indexes as (
         select i.indexrelid
         from
             pg_catalog.pg_constraint c
+            inner join nsp on nsp.oid = c.connamespace
             inner join lateral unnest(c.conkey) with ordinality u(attnum, attposition) on true
             inner join pg_catalog.pg_index i on i.indrelid = c.conrelid and (c.conkey::int[] <@ i.indkey::int[])
         where c.contype = 'f'
@@ -28,7 +38,7 @@ from
     pg_catalog.pg_stat_user_indexes psui
     inner join pg_catalog.pg_index i on i.indexrelid = psui.indexrelid
 where
-    psui.schemaname = :schema_name_param::text and
+    psui.schemaname in (select nspname from nsp) and
     not i.indisunique and
     i.indexrelid not in (select * from foreign_key_indexes) and /* retain indexes on foreign keys */
     psui.idx_scan < 50::integer
