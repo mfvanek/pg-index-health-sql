cb0720d2961319233e9d6abc23c3dc45fcade6a5
dgilfanova <gilfanova.dianka@mail.ru>
2025-04-17 22:47:11 +0300

    Add check "Don't use varchar(n) by default"

---
+++ sql/columns_with_fixed_length_varchar.sql
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2019-2025. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health-sql
+ *
+ * Licensed under the Apache License 2.0
+ */
+
+-- Finds columns of type 'varchar(n)'.
+--
+-- See also https://wiki.postgresql.org/wiki/Don%27t_Do_This#Don.27t_use_varchar.28n.29_by_default
+select
+    t.oid::regclass::text as table_name,
+    col.attnotnull as column_not_null,
+    quote_ident(col.attname) as column_name
+from
+    pg_catalog.pg_class t
+    inner join pg_catalog.pg_namespace nsp on t.relnamespace = nsp.oid
+    inner join pg_catalog.pg_attribute col on t.oid = col.attrelid
+where
+    t.relkind in ('r', 'p') and
+    not t.relispartition and
+    col.attnum > 0 and /* to filter out system columns such as oid, ctid, xmin, xmax, etc. */
+    not col.attisdropped and
+    col.atttypid = 'varchar'::regtype and
+    col.atttypmod > 0 and /* only for fixed length varchar (not varchar without n) */
+    nsp.nspname = :schema_name_param::text
+order by table_name, column_name;
